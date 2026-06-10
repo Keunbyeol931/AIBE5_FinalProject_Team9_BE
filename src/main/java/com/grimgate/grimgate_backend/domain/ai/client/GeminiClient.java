5b9e88eb57b6a4b21cb649c34a05f21fc30ac858
LeeGoneHee <sjwwiies2829@gmail.com>
2026-06-10 11:42:11 +0900

    Update GeminiClient.java

--- src/main/java/com/grimgate/grimgate_backend/domain/ai/client/GeminiClient.java
+++ src/main/java/com/grimgate/grimgate_backend/domain/ai/client/GeminiClient.java
@@ -47,8 +47,7 @@ public class GeminiClient {
         );
 
 
-        System.out.println("MODEL = " + model);
-        System.out.println("API KEY 앞 5자리 = " + apiKey.substring(0, 5));
+
        try{
         String response = webClient.post()
                 .uri(uriBuilder -> uriBuilder
